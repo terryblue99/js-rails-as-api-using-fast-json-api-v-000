fe4d01255a5e97d60933e7c3418d50d187887d29
Terry Blue <terryblue@comcast.net>
2020-02-16 01:58:01 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -4,7 +4,7 @@ class SightingsController < ApplicationController
     sightings = Sighting.all
     render json: SightingSerializer.new(sightings)
   end
-  
+
   def show
     @sighting = Sighting.find(params[:id])
     render json: SightingSerializer.new(sighting)
